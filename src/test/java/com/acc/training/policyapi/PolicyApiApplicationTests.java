3449b8d5d1396631084d5ecdc4ee4aff50c51499
ankurbhadoria <ankur.bhadoria@gmail.com>
2021-01-15 00:06:41 -0500

    Spring Initializr for policy-api

---
+++ src/test/java/com/acc/training/policyapi/PolicyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.training.policyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
