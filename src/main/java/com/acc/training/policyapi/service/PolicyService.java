d769044f436623e98f8d47d1aa08dc89700c77fa
sandeepp873 <69438787+sandeepp873@users.noreply.github.com>
2021-02-12 00:20:42 -0500

    Customer service Connectivity
    Customer service Connectivity

--- src/main/java/com/acc/training/policyapi/service/PolicyService.java
+++ src/main/java/com/acc/training/policyapi/service/PolicyService.java
@@ -1,16 +1,21 @@
 package com.acc.training.policyapi.service;
 
+import com.acc.training.policyapi.model.Customer;
 import com.acc.training.policyapi.model.Policy;
 import com.acc.training.policyapi.repository.IPolicyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PolicyService {
 
     @Autowired
     IPolicyRepository policyRepository;
+
+    @Autowired
+    WebClient customerWebClient;
 
     public Policy createPolicy(Policy policy) {
         return policyRepository.save(policy);
@@ -19,11 +24,17 @@ public class PolicyService {
     public Policy getPolicy(String policyId) {
         Policy result = policyRepository.findByPolicyId(policyId);
 
-        //TODO: add web-client code for microservice communication
-
         if (result == null) {
             return null;
         }
+
+        String customerId = result.getCustomer().getCustomerId();
+
+        Customer customer = customerWebClient.get().uri("/customer/"+customerId).retrieve().bodyToMono(Customer.class).block();
+        if (customer != null){
+            result.setCustomer(customer);
+        }
+
         return result;
     }
 
