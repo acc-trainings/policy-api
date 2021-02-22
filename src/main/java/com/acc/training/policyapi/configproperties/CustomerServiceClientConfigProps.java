799a734d1728f5791b88990b3d7e2ad36e6c8130
Manuj Chugh <manujchugh@gmail.com>
2021-02-22 16:07:20 -0500

    Changes to policy api to call customer api

---
+++ src/main/java/com/acc/training/policyapi/configproperties/CustomerServiceClientConfigProps.java
@@ -0,0 +1,19 @@
+package com.acc.training.policyapi.configproperties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("external.services.customer")
+public class CustomerServiceClientConfigProps {
+
+    private String uri;
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
--- src/main/java/com/acc/training/policyapi/service/PolicyService.java
+++ src/main/java/com/acc/training/policyapi/service/PolicyService.java
@@ -1,10 +1,12 @@
 package com.acc.training.policyapi.service;
 
+import com.acc.training.policyapi.model.Customer;
 import com.acc.training.policyapi.model.Policy;
 import com.acc.training.policyapi.repository.IPolicyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PolicyService {
@@ -12,6 +14,9 @@ public class PolicyService {
     @Autowired
     IPolicyRepository policyRepository;
 
+    @Autowired
+    WebClient customerWebClient;
+    
     public Policy createPolicy(Policy policy) {
         return policyRepository.save(policy);
     }
@@ -19,11 +24,15 @@ public class PolicyService {
     public Policy getPolicy(String policyId) {
         Policy result = policyRepository.findByPolicyId(policyId);
 
-        //TODO: add web-client code for microservice communication
-
+        
         if (result == null) {
             return null;
         }
+
+        String customerId= result.getCustomer().getCustomerId();
+
+        Customer customer = customerWebClient.get().uri("/customer/"+customerId).retrieve().bodyToMono(Customer.class).block();
+        result.setCustomer(customer);
         return result;
     }
 
