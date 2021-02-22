4cc7840f7c3ce2dc3aa4f56762b00506fabeb4d6
y.cao@accenture.com <y.cao@accenture.com>
2021-02-22 16:09:41 -0500

    commit training 02-22

---
+++ src/main/java/com/acc/training/policyapi/config/HttpHeaders.java
@@ -0,0 +1,5 @@
+package com.acc.training.policyapi.config;
+
+public enum HttpHeaders {
+
+}
--- src/main/java/com/acc/training/policyapi/service/PolicyService.java
+++ src/main/java/com/acc/training/policyapi/service/PolicyService.java
@@ -1,16 +1,21 @@
 package com.acc.training.policyapi.service;
 
+import com.acc.training.policyapi.model.Customer;
 import com.acc.training.policyapi.model.Policy;
 import com.acc.training.policyapi.repository.IPolicyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PolicyService {
 
     @Autowired
     IPolicyRepository policyRepository;
+
+    @Autowired 
+    WebClient customerWebClient;
 
     public Policy createPolicy(Policy policy) {
         return policyRepository.save(policy);
@@ -24,6 +29,11 @@ public class PolicyService {
         if (result == null) {
             return null;
         }
+
+        String customerId = result.getCustomer().getCustomerId();
+
+        Customer customer = customerWebClient.get().uri("/customer/"+customerId).retrieve().bodyToMono(Customer.class).block();
+        result.setCustomer(customer);
         return result;
     }
 
