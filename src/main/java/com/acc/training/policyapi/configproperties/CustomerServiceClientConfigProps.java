c14f1e8b9cae102c5f8226ae987d28b94528c154
Jayesh <Jayesh82mscit@gmail.com>
2021-02-22 21:15:29 +0000

    PolicyAPI changes

---
+++ src/main/java/com/acc/training/policyapi/configproperties/CustomerServiceClientConfigProps.java
@@ -0,0 +1,18 @@
+package com.acc.training.policyapi.configproperties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("external.services.customer")
+public class CustomerServiceClientConfigProps {
+    private String uri;
+
+    public String getUri() {
+        return this.uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
